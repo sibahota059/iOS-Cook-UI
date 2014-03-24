c5d36898a50274894f1cf03cd6a95f3f2103cadf
xp19870106 <xp19870106@gmail.com>
2014-03-24 13:30:00 +0800

    add UIColor category to get a colour from hex string

---
+++ iOS-Cook-UI/Category/UIColor+Category.h
@@ -0,0 +1,15 @@
+//
+//  UIColor+Category.h
+//  iOS-Cook-UI
+//
+//  Created by XP on 3/24/14.
+//  Copyright (c) 2014 newland. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIColor (Category)
+
++ (UIColor *) colorFromHexString:(NSString *)hexString;
+
+@end
---
+++ iOS-Cook-UI/Category/XPCategory.h
@@ -0,0 +1,11 @@
+//
+//  XPCategory.h
+//  iOS-Cook-UI
+//
+//  Created by XP on 3/24/14.
+//  Copyright (c) 2014 newland. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "UIColor+Category"
