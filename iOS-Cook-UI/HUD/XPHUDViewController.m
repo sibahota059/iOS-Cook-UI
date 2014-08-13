f96becd6f3300c6a8b8bacbcb93df332574bbc67
xp19870106 <xp19870106@gmail.com>
2014-08-13 22:01:59 +0800

    hud amend

--- iOS-Cook-UI/HUD/XPHUDViewController.m
+++ iOS-Cook-UI/HUD/XPHUDViewController.m
@@ -7,15 +7,17 @@
 //
 
 #import "XPHUDViewController.h"
-#import "XPHUD.h"
-#import "AFNetworking.h"
+#import "MBProgressHUD.h"
 
 static NSString *CellIdentifier = @"Cell";
 
-@interface XPHUDViewController ()
+@interface XPHUDViewController ()<MBProgressHUDDelegate> {
+	MBProgressHUD *HUD;
+    long long expectedLength;
+	long long currentLength;
+}
 
 @property (nonatomic, strong) NSArray *hudArray;
-@property (nonatomic, strong) XPHUD *hud;
 
 @end
 
@@ -35,7 +37,20 @@ static NSString *CellIdentifier = @"Cell";
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:CellIdentifier];
-    self.hudArray = @[@"Show activity only",@"Show caption only",@"show activity with a caption"];
+    self.hudArray = @[@"Simple indeterminate progress",
+                      @"With lable",
+                      @"With details label",
+                      @"Determinate mode",
+                      @"Annular determinate mode",
+                      @"Bar determinate mode",
+                      @"Custom view",
+                      @"Mode switching",
+                      @"Using blocks",
+                      @"On Window",
+                      @"NSURLConnection",
+                      @"Dim background",
+                      @"Text only",
+                      @"Colored"];
 }
 
 - (void)didReceiveMemoryWarning
@@ -61,66 +76,314 @@ static NSString *CellIdentifier = @"Cell";
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     [self.tableView deselectRowAtIndexPath:indexPath animated:YES];
-    
     switch (indexPath.row) {
         case 0:
-        {
-            self.hud = [[XPHUD alloc] initWithTitle:nil type:XPHUDTypeActivityIndicatorOnly image:nil];
-            [self.hud showInView:self.view];
-            AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-            manager.responseSerializer.acceptableContentTypes = [manager.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
-            [manager GET:@"http://m.weather.com.cn/data/101010100.html" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                [self.hud remove];
-                self.hud = [[XPHUD alloc] initWithTitle:nil type:XPHUDTypeCustomImageOnly image:[UIImage imageNamed:@"Succeed"]];
-                [self.hud showInView:self.view hideAfter:3.0];
-            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                [self.hud remove];
-                self.hud = [[XPHUD alloc] initWithTitle:nil type:XPHUDTypeCustomImageOnly image:[UIImage imageNamed:@"Fail"]];
-                [self.hud showInView:self.view hideAfter:3.0];
-            }];
-        }
+            [self showSimple];
             break;
         case 1:
-        {
-            self.hud = [[XPHUD alloc] initWithTitle:@"正在发起网络请求，请稍等" type:XPHUDTypeCaptionOnly image:nil];
-            [self.hud showInView:self.view];
-            AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-            manager.responseSerializer.acceptableContentTypes = [manager.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
-            [manager GET:@"http://m.weather.com.cn/data/101010100.html" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                [self.hud remove];
-                self.hud = [[XPHUD alloc] initWithTitle:nil type:XPHUDTypeCustomImageOnly image:[UIImage imageNamed:@"Succeed"]];
-                [self.hud showInView:self.view hideAfter:3.0];
-            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                [self.hud remove];
-                self.hud = [[XPHUD alloc] initWithTitle:nil type:XPHUDTypeCustomImageOnly image:[UIImage imageNamed:@"Fail"]];
-                [self.hud showInView:self.view hideAfter:3.0];
-            }];
-        }
+            [self showWithLabel];
             break;
         case 2:
-        {
-            self.hud = [[XPHUD alloc] initWithTitle:@"正在发起网络请求，请稍等" type:XPHUDTypeActivityWithCaption image:nil];
-            [self.hud showInView:self.view];
-            AFHTTPRequestOperationManager *manager = [AFHTTPRequestOperationManager manager];
-            manager.responseSerializer.acceptableContentTypes = [manager.responseSerializer.acceptableContentTypes setByAddingObject:@"text/html"];
-            [manager GET:@"http://m.weather.com.cn/data/101010100.html" parameters:nil success:^(AFHTTPRequestOperation *operation, id responseObject) {
-                [self.hud remove];
-                self.hud = [[XPHUD alloc] initWithTitle:nil type:XPHUDTypeCustomImageOnly image:[UIImage imageNamed:@"Succeed"]];
-                [self.hud showInView:self.view hideAfter:3.0];
-            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-                [self.hud remove];
-                self.hud = [[XPHUD alloc] initWithTitle:nil type:XPHUDTypeCustomImageOnly image:[UIImage imageNamed:@"Fail"]];
-                [self.hud showInView:self.view hideAfter:3.0];
-            }];
-        }
+            [self showWithDetailsLabel];
+            break;
+        case 3:
+            [self showWithLabelDeterminate];
+            break;
+        case 4:
+            [self showWithLabelAnnularDeterminate];
+            break;
+        case 5:
+            [self showWithLabelDeterminateHorizontalBar];
+            break;
+        case 6:
+            [self showWithCustomView];
+            break;
+        case 7:
+            [self showWithLabelMixed];
+            break;
+        case 8:
+            [self showUsingBlocks];
+            break;
+        case 9:
+            [self showOnWindow];
+            break;
+        case 10:
+            [self showURL];
+            break;
+        case 11:
+            [self showWithGradient];
+            break;
+        case 12:
+            [self showTextOnly];
+            break;
+        case 13:
+            [self showWithColor];
+            break;
         default:
             break;
     }
 }
 
+#pragma mark - Actions
+
+- (void)showSimple {
+	// The hud will dispable all input on the view (use the higest view possible in the view hierarchy)
+	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+	[self.navigationController.view addSubview:HUD];
+	
+	// Regiser for HUD callbacks so we can remove it from the window at the right time
+	HUD.delegate = self;
+	
+	// Show the HUD while the provided method executes in a new thread
+	[HUD showWhileExecuting:@selector(myTask) onTarget:self withObject:nil animated:YES];
+}
+
+- (void)showWithLabel {
+	
+	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+	[self.navigationController.view addSubview:HUD];
+	
+	HUD.delegate = self;
+	HUD.labelText = @"Loading";
+	
+	[HUD showWhileExecuting:@selector(myTask) onTarget:self withObject:nil animated:YES];
+}
+
+- (void)showWithDetailsLabel {
+	
+	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+	[self.navigationController.view addSubview:HUD];
+	
+	HUD.delegate = self;
+	HUD.labelText = @"Loading";
+	HUD.detailsLabelText = @"updating data";
+	HUD.square = YES;
+	
+	[HUD showWhileExecuting:@selector(myTask) onTarget:self withObject:nil animated:YES];
+}
+
+- (void)showWithLabelDeterminate {
+	
+	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+	[self.navigationController.view addSubview:HUD];
+	
+	// Set determinate mode
+	HUD.mode = MBProgressHUDModeDeterminate;
+	
+	HUD.delegate = self;
+	HUD.labelText = @"Loading";
+	
+	// myProgressTask uses the HUD instance to update progress
+	[HUD showWhileExecuting:@selector(myProgressTask) onTarget:self withObject:nil animated:YES];
+}
+
+- (void)showWithLabelAnnularDeterminate {
+	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+	[self.navigationController.view addSubview:HUD];
+	
+	// Set determinate mode
+	HUD.mode = MBProgressHUDModeAnnularDeterminate;
+	
+	HUD.delegate = self;
+	HUD.labelText = @"Loading";
+	
+	// myProgressTask uses the HUD instance to update progress
+	[HUD showWhileExecuting:@selector(myProgressTask) onTarget:self withObject:nil animated:YES];
+}
+
+- (void)showWithLabelDeterminateHorizontalBar {
+	
+	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+	[self.navigationController.view addSubview:HUD];
+	
+	// Set determinate bar mode
+	HUD.mode = MBProgressHUDModeDeterminateHorizontalBar;
+	
+	HUD.delegate = self;
+	
+	// myProgressTask uses the HUD instance to update progress
+	[HUD showWhileExecuting:@selector(myProgressTask) onTarget:self withObject:nil animated:YES];
+}
+
+- (void)showWithCustomView {
+	
+	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+	[self.navigationController.view addSubview:HUD];
+	
+	// The sample image is based on the work by http://www.pixelpressicons.com, http://creativecommons.org/licenses/by/2.5/ca/
+	// Make the customViews 37 by 37 pixels for best results (those are the bounds of the build-in progress indicators)
+	HUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"37x-Checkmark.png"]] ;
+	
+	// Set custom view mode
+	HUD.mode = MBProgressHUDModeCustomView;
+	
+	HUD.delegate = self;
+	HUD.labelText = @"Completed";
+	
+	[HUD show:YES];
+	[HUD hide:YES afterDelay:3];
+}
+
+- (void)showWithLabelMixed {
+	
+	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+	[self.navigationController.view addSubview:HUD];
+	
+	HUD.delegate = self;
+	HUD.labelText = @"Connecting";
+	HUD.minSize = CGSizeMake(135.f, 135.f);
+	
+	[HUD showWhileExecuting:@selector(myMixedTask) onTarget:self withObject:nil animated:YES];
+}
+
+- (void)showUsingBlocks {
+	MBProgressHUD *hud = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+	[self.navigationController.view addSubview:hud];
+	hud.labelText = @"With a block";
+	
+	[hud showAnimated:YES whileExecutingBlock:^{
+		[self myTask];
+	} completionBlock:^{
+		[hud removeFromSuperview];
+	}];
+}
+
+- (void)showOnWindow {
+	// The hud will dispable all input on the window
+	HUD = [[MBProgressHUD alloc] initWithView:self.view.window];
+	[self.view.window addSubview:HUD];
+	
+	HUD.delegate = self;
+	HUD.labelText = @"Loading";
+	
+	[HUD showWhileExecuting:@selector(myTask) onTarget:self withObject:nil animated:YES];
+}
+
+- (void)showURL {
+	NSURL *URL = [NSURL URLWithString:@"http://a1408.g.akamai.net/5/1408/1388/2005110403/1a1a1ad948be278cff2d96046ad90768d848b41947aa1986/sample_iPod.m4v.zip"];
+	NSURLRequest *request = [NSURLRequest requestWithURL:URL];
+	
+	NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:self];
+	[connection start];
+	
+	HUD = [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
+	HUD.delegate = self;
+}
+
+
+- (void)showWithGradient {
+	
+	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+	[self.navigationController.view addSubview:HUD];
+	
+	HUD.dimBackground = YES;
+	
+	// Regiser for HUD callbacks so we can remove it from the window at the right time
+	HUD.delegate = self;
+	
+	// Show the HUD while the provided method executes in a new thread
+	[HUD showWhileExecuting:@selector(myTask) onTarget:self withObject:nil animated:YES];
+}
+
+- (void)showTextOnly {
+	
+	MBProgressHUD *hud = [MBProgressHUD showHUDAddedTo:self.navigationController.view animated:YES];
+	
+	// Configure for text only and offset down
+	hud.mode = MBProgressHUDModeText;
+	hud.labelText = @"Some message...";
+	hud.margin = 10.f;
+	hud.removeFromSuperViewOnHide = YES;
+	
+	[hud hide:YES afterDelay:3];
+}
+
+- (void)showWithColor{
+	HUD = [[MBProgressHUD alloc] initWithView:self.navigationController.view];
+	[self.navigationController.view addSubview:HUD];
+	
+	// Set the hud to display with a color
+	HUD.color = [UIColor colorWithRed:0.23 green:0.50 blue:0.82 alpha:0.90];
+	
+	HUD.delegate = self;
+	[HUD showWhileExecuting:@selector(myTask) onTarget:self withObject:nil animated:YES];
+}
+
+#pragma mark - Execution code
+
 - (void)myTask {
 	// Do something usefull in here instead of sleeping ...
 	sleep(3);
+}
+
+- (void)myProgressTask {
+	// This just increases the progress indicator in a loop
+	float progress = 0.0f;
+	while (progress < 1.0f) {
+		progress += 0.01f;
+		HUD.progress = progress;
+		usleep(50000);
+	}
+}
+
+- (void)myMixedTask {
+	// Indeterminate mode
+	sleep(2);
+	// Switch to determinate mode
+	HUD.mode = MBProgressHUDModeDeterminate;
+	HUD.labelText = @"Progress";
+	float progress = 0.0f;
+	while (progress < 1.0f)
+	{
+		progress += 0.01f;
+		HUD.progress = progress;
+		usleep(50000);
+	}
+	// Back to indeterminate mode
+	HUD.mode = MBProgressHUDModeIndeterminate;
+	HUD.labelText = @"Cleaning up";
+	sleep(2);
+	// UIImageView is a UIKit class, we have to initialize it on the main thread
+	__block UIImageView *imageView;
+	dispatch_sync(dispatch_get_main_queue(), ^{
+		UIImage *image = [UIImage imageNamed:@"37x-Checkmark.png"];
+		imageView = [[UIImageView alloc] initWithImage:image];
+	});
+	HUD.customView = imageView;
+	HUD.mode = MBProgressHUDModeCustomView;
+	HUD.labelText = @"Completed";
+	sleep(2);
+}
+
+#pragma mark - NSURLConnectionDelegete
+
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+	expectedLength = MAX([response expectedContentLength], 1);
+	currentLength = 0;
+	HUD.mode = MBProgressHUDModeDeterminate;
+}
+
+- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
+	currentLength += [data length];
+	HUD.progress = currentLength / (float)expectedLength;
+}
+
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+	HUD.customView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"37x-Checkmark.png"]];
+	HUD.mode = MBProgressHUDModeCustomView;
+	[HUD hide:YES afterDelay:2];
+}
+
+- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
+	[HUD hide:YES];
+}
+
+#pragma mark - MBProgressHUDDelegate
+
+- (void)hudWasHidden:(MBProgressHUD *)hud {
+	// Remove HUD from screen when the HUD was hidded
+	[HUD removeFromSuperview];
+	HUD = nil;
 }
 
 
